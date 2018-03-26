21d32224745983eae493ecdf92ecd37663e73afa
张玉涛 <zhangyutao@tomato.shop>
2018-03-26 09:44:42 +0800

    update

--- orange-mybatis/src/main/java/com/delicacy/orange/mybatis/v1/MapperXml.java
+++ orange-mybatis/src/main/java/com/delicacy/orange/mybatis/v1/MapperXml.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MapperXml {
-    public static final String nameSpace ="com.delicacy.orange.mybatis.v1.Mapper";
+    public static final String nameSpace ="com.delicacy.orange.mybatis.arraylist.Mapper";
     public static final Map<String, String> methodSqlMapping =  new HashMap<>();
     static {
-        methodSqlMapping.put("selectOne","select *  from t_emp where id = ?");
+        methodSqlMapping.put("selectOne","select * from t_emp where id = ?");
     }
     public static String getMethodSql(String statement){
        return methodSqlMapping.get(statement);
